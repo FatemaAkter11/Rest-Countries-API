1bdcb912f2fe5209537582a326c79d1d2ea497c1
FatemaAkter11 <fatemaakter4261@gmail.com>
2021-09-01 00:14:50 +0600

    Rest-Countries-API

--- js/countries.js
+++ js/countries.js
@@ -12,12 +12,34 @@ const displayCountries = countries => {
         // console.log(country);
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        div.appendChild(h3);
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        div.appendChild(p);
+        /*   const h3 = document.createElement('h3');
+         h3.innerText = country.name;
+         div.appendChild(h3);
+         const p = document.createElement('p');
+         p.innerText = country.capital;
+         div.appendChild(p); */
+        div.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick=loadCountryName('${country.name}')>Details</button>
+        `;
         countriesDiv.appendChild(div);
     });
+}
+const loadCountryName = name => {
+    // console.log(name);
+    const url = `https://restcountries.eu/rest/v2/name/${name}`;
+    // console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => loadCountryDetails(data[0]));
+}
+const loadCountryDetails = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+        <h3>${country.name}</h3>
+        <p>${country.population}</p>
+        <img src="${country.flag}">
+`;
 }
